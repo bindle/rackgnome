d650211777b7c4d5106b9c20b24f283c2cddfebb
David M. Syzdek <syzdek@bindlebinaries.com>
2013-09-28 10:08:23 -0800

    Adding initial start of schema draft.

---
+++ doc/design-requirements.md
@@ -0,0 +1,14 @@
+
+Rack Gnome Design Requirements
+==============================
+
+   1. Documentation
+      1. Location (Building/Room/Row/Rack/Shelf)
+      2. Network
+      3. Power
+      4. Console
+      5. Hardware
+      6. Software
+   2. Interface
+   3. Development
+
---
+++ doc/rack-gnome-schema.sql
@@ -0,0 +1,107 @@
+/*
+ *  Rack Gnome
+ *  Copyright (C) 2013 Bindle Binaries
+ */
+/*
+ *  Run:
+ *     cat rack-gnome-schema.sql | sqlite3 rackgnome.db
+ */
+
+
+/*
+ *  Scope:         ISP
+ *  Domain:        rackgnome.org
+ */
+
+
+/*
+ *  System:        eagle.anc.ak
+ *  Scope:         ISP
+ *  OS:            Linux/Slackware
+ *  OS Version:    3.8.4/14.0
+ *  Installed:     2013/09/28 by syzdek
+ *  Patched:       2013/09/28 by syzdek
+ *  Device Group:  smtp farm
+ *  Guest Host:
+ *  Guest Type:
+ *  Primary:       syzdek
+ *  Secondary:     jones
+ *  Comment:
+ */
+
+
+CREATE TABLE version
+(
+   id             SERIAL PRIMARY KEY,
+   uuid           UUID UNIQUE NOT NULL,
+   major          INTEGER,
+   minor          INTEGER,
+   patch          INTEGER,
+   build          INTEGER,
+   tag            VARCHAR(128),
+   string         VARCHAR(128)
+);
+
+
+CREATE TABLE os
+(
+   id             SERIAL PRIMARY KEY,
+   uuid           UUID UNIQUE NOT NULL,
+   name           VARCHAR(128) NOT NULL,
+   description    VARCHAR(128),
+   hasVersion     BOOL DEAFAULT TRUE,
+   osVendorId     INTEGER,
+   FOREIGN KEY (osVendorId) REFERENCES osVendor (id)
+);
+
+
+CREATE TABLE osVersion
+(
+   osId           INTEGER,
+   versionId      INTEGER,
+   PRIMARY KEY (osId, versionId)
+   FOREIGN KEY (osId)      REFERENCES os      (id),
+   FOREIGN KEY (versionId) REFERENCES version (id)
+);
+
+
+CREATE TABLE osVendor
+(
+   id             SERIAL PRIMARY KEY,
+   uuid           UUID UNIQUE NOT NULL,
+   name           VARCHAR(128) NOT NULL,
+   description    VARCHAR(128),
+   hasVersion     BOOL DEFAULT FALSE
+);
+
+
+CREATE TABLE osVendorVersion
+(
+   osVendorId     INTEGER,
+   versionId      INTEGER,
+   PRIMARY KEY (osVendorId, versionId)
+   FOREIGN KEY (osVendorId) REFERENCES osVendor (id),
+   FOREIGN KEY (versionId)  REFERENCES version  (id)
+);
+
+
+CREATE TABLE device
+(
+   id               SERIAL PRIMARY KEY,
+   uuid             UUID UNIQUE NOT NULL,
+   name             VARCHAR(128) NOT NULL,
+   description      VARCHAR(1024),
+   hostname         VARCHAR(128),
+   uname            VARCHAR(128),
+   osId             INTEGER,
+   osVerId          INTEGER,
+   osVendorId       INTEGER,
+   osVendorVerId    INTEGER,
+   FOREIGN KEY (osId)          REFERENCES os              (id),
+   FOREIGN KEY (osVerId)       REFERENCES osVersion       (id),
+   FOREIGN KEY (osVendorId)    REFERENCES osVendor        (id),
+   FOREIGN KEY (osVendorVerId) REFERENCES osVendorVersion (id)
+);
+
+
+/* end of schema */
