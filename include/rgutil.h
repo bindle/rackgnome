45717cdfb628dee0f7105d7dc462c2d5d19c5f89
David M. Syzdek <syzdek@bindlebinaries.com>
2016-08-24 20:11:37 -0800

    Re-odering prototypes in rgutil.h

--- include/rgutil.h
+++ include/rgutil.h
@@ -90,9 +90,9 @@ struct rgutil_pkg_info
 #pragma mark -
 #endif
 
+_RACKGNOME_F void rgutil_version(rackgnome_ver const ** verp);
 _RACKGNOME_F void rgutil_version_print(const char * prog_name);
 _RACKGNOME_F void rgutil_version_print_terse(const char * prog_name);
-_RACKGNOME_F void rgutil_version(rackgnome_ver const ** verp);
 _RACKGNOME_F void rgutil_clock_gettime(struct timespec * ts);
 
 
